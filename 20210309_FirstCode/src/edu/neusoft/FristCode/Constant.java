b4c1cf60facfa25bd5aadb08938d9a9dd7084fa7
KAKUKA86 <zhangbang2002@foxmail.com>
2021-03-09 20:11:23 +0800

    New Code

---
+++ 20210309_FirstCode/src/edu/neusoft/FristCode/Constant.java
@@ -0,0 +1,11 @@
+package edu.neusoft.FristCode;
+
+public class Constant 
+{
+	public static void main(String[] args)
+	{
+		final int a = 4;
+		final int b = 4;
+		System.out.println(a+b);
+	}
+}
---
+++ 20210309_FirstCode/src/edu/neusoft/FristCode/variable.java
@@ -0,0 +1,13 @@
+package edu.neusoft.FristCode;
+
+public class variable
+{
+	public static void main(String[] args)
+	{
+		int a = 0;
+		int b = 0;
+		a = 3;
+		b = 4;
+		System.out.println(a+b);
+	}
+}
